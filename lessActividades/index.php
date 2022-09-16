c3beb9a62e61a1e3f24c4ebdf46b5917a96d2976
JesusSepulvedaLopez <jese_18@alu.uabcs.mx>
2022-09-15 18:42:06 -0600

    less

---
+++ lessActividades/index.php
@@ -0,0 +1,35 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Panel</title>
+    <link rel="stylesheet" type="text/css" href="public/css/main.css">
+</head>
+
+<body>
+    <div class="container">
+
+        <fieldset>
+            <legend>
+                Acceso al panel
+            </legend>
+            
+            <form action="">
+                <label for="">Correo electronico</label>
+                <input type="email">
+                <br>
+                <label for="">Contraseña</label>
+                <input type="password">
+                <br>
+                <button type="submit">Acceder</button>
+            </form>
+
+        </fieldset>
+
+    </div>
+</body>
+
+</html>
